6c92b350afcf5758eec9df10b833db68a28dbae3
Harsh-Duche <ducheharsh@gmail.com>
2025-09-28 05:37:14 +0530

    feat: exposed all routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,12 @@
 import { Hono } from "hono";
 import devRoute from "./dev.route";
+import agentRoute from "./agent.route";
+import apiKeyRoute from "./apiKey.route";
 
 const app = new Hono();
 
 app.route('/', devRoute);
+app.route('/dagent', agentRoute);
+app.route('/apikey', apiKeyRoute);
 
 export default app;
